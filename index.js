2095d6068b75f466e68dc35ef8b04b1b64a691c8
abdullah-al-mohammad <abdullahalmuhammad6@gmail.com>
2025-09-03 10:13:05 +0600

    update user admin role

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const port = process.env.PORT || 5000
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 var jwt = require('jsonwebtoken');
+const { log } = require('console')
 
 // middleware
 app.use(cors())
@@ -36,15 +37,19 @@ async function run() {
     app.post('/jwt', async (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+      
       res.send({token})
     })
     // middleWare for token verify
     const verifyToken = (req, res, next) => {
+      const authHeader = req.headers.authorization;
+      console.log(authHeader);
       
-      if (!req.headers.authorization) {
-        return res.status(401).send({ message: "unauthorized Access" });
-      }
-      const token = req.headers.authorization.split(' ')[1]
+      
+      if (!authHeader) {
+				return res.status(401).send({ message: "unauthorized Access" });
+			}
+      const token = authHeader.split(" ")[1];
       jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if (err) {
             return res.status(401).send({ message: "unauthorized access" });
@@ -58,11 +63,9 @@ async function run() {
 // user related apis request
     app.post('/users', async(req,res) => {
       const user = req.body
-      console.log(user);
       
       // if user already exist
       const query = { email: user?.email }
-      console.log(query);
       
       const existingUser = await userCollection.findOne(query);
       if(existingUser){
@@ -74,7 +77,6 @@ async function run() {
         status: 'active'
       }
       const result = await userCollection.insertOne(newUser)
-      console.log(result);
       
       res.send(result)
     })
@@ -86,47 +88,42 @@ async function run() {
       res.send(result)
     })
 // make admin
-    // app.get('/users/admin/:email', verifyToken, async (req, res) => {
-    //   const email = req.params.email;
-    //   if (email !== req?.decoded?.email) {
-    //      return res.status(403).send({ message: "forbidden access" });
-    //   }
-    //   const query = { email: email }
-    //   const user = await userCollection.findOne(query);
-    //   let admin = false
-    //   if (user) {
-    //     admin = user?.role === 'admin'
-    //   }
-    //   res.send({admin})
-    // })
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req?.decoded?.email) {
+         return res.status(403).send({ message: "forbidden access" });
+      }
+      const query = { email: email }
+      const user = await userCollection.findOne(query);
+      let admin = false
+      if (user) {
+        admin = user?.role === "admin"
+      }
+      res.send({admin})
+    })
 
-    // app.get('/users/:id', verifyToken, async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: new ObjectId(id) }
-    //   const result = await userCollection.findOne(query)
-    //   // console.log(result);
-    //   res.send(result)
-    // })
-    // app.patch('/users/:id', async(req, res) => {
-    //   const user = req.body
-    //   const id = req.params.id
-    //   const filter = {_id: new ObjectId(id)}
+    app.get('/users/:id', verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await userCollection.findOne(query)
+      res.send(result)
+    })
+    app.patch('/users/:id', async(req, res) => {
+      const user = req.body
+      const id = req.params.id
+      const filter = {_id: new ObjectId(id)}
 
-    //   const updateDoc = { $set: {} };
+      const updateDoc = { $set: {} };
 
-    //     // Update only the fields that are present in the request
-    //     if (user.name) updateDoc.$set.name = user.name;
-    //     if (user.district) updateDoc.$set.district = user.district;
-    //     if (user.subDistrict) updateDoc.$set.subDistrict = user.subDistrict;
-    //     if (user.blood) updateDoc.$set.blood = user.blood;
-    //     if (user.image) updateDoc.$set.image = user.image;
-    //     if (user.status) updateDoc.$set.status = user.status;
-    //     if (user.role) updateDoc.$set.role = user.role;
+        // Update only the fields that are present in the request
+        if (user.role) updateDoc.$set.role = user.role;
 
 
-    //   const result = await userCollection.updateOne(filter, updateDoc)
-    //   res.send(result)
-    // })
+      const result = await userCollection.updateOne(filter, updateDoc)
+      console.log(result);
+      
+      res.send(result)
+    })
     
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });

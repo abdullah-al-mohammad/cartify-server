2a7b08d00549b3b7b60a58723d9b652106a56ab3
abdullah-al-mohammad <abdullahalmuhammad6@gmail.com>
2025-10-16 14:00:54 +0600

    authmiddleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,38 +1,34 @@
-const jwt = require("jsonwebtoken");
-const { getCollection } = require("../config/db");
+const jwt = require('jsonwebtoken');
+const { getCollection } = require('../config/db');
 
 const verifyToken = (req, res, next) => {
-	const authHeader = req.headers.authorization;
-	
-	
-	if (!authHeader)
-		return res.status(401).json({ message: "Unauthorized: No token provided" });
+  const authHeader = req.headers.authorization;
 
-	const token = authHeader.split(" ")[1];
-	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-		if (err)
-			return res.status(401).json({ message: "Unauthorized: Invalid token" });
+  if (!authHeader) return res.status(401).json({ message: 'Unauthorized: No token provided' });
 
-		req.decoded = decoded;
-		next();
-	});
+  const token = authHeader.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) return res.status(401).json({ message: 'Unauthorized: Invalid token' });
+
+    req.decoded = decoded;
+    next();
+  });
 };
 
 const verifyAdmin = async (req, res, next) => {
-	try {
-		const userCollection = getCollection("customerDb", "users");
-		const email = req?.decoded?.email;
-		const user = await userCollection.findOne({ email });
+  try {
+    const userCollection = getCollection('customerDb', 'users');
+    const email = req?.decoded?.email;
+    const user = await userCollection.findOne({ email });
 
-		if (user?.role !== "admin") {
-			return res
-				.status(403)
-				.json({ message: "Forbidden: Admin access required" });
-		}
-		next();
-	} catch (error) {
-		next(error);
-	}
+    if (user?.role !== 'admin') {
+      return res.status(403).json({ message: 'Forbidden: Admin access required' });
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = { verifyToken, verifyAdmin };

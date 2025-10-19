2350bf8c9a3a33e4f3e8d78a956ab3c7ffb1d5f1
abdullah-al-mohammad <abdullahalmuhammad6@gmail.com>
2025-10-19 12:41:45 +0600

    add cors new line

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-const express = require('express')
-require('dotenv').config()
-const app = express()
-const cors = require('cors')
-const { connectDB } = require("./config/db");
-const port = process.env.PORT || 5000
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const cors = require('cors');
+const { connectDB } = require('./config/db');
+const port = process.env.PORT || 5000;
 
-const userRoutes = require("./routes/userRoutes");
-const productRoutes = require("./routes/productRoutes");
-const orderRoutes = require("./routes/orderRoutes");
-const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
-
+const userRoutes = require('./routes/userRoutes');
+const productRoutes = require('./routes/productRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
 
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors({ origin: '*' }));
+app.use(express.json());
 app.use(express.text());
 
 connectDB();
 
-app.use("/users", userRoutes);
-app.use("/products", productRoutes);
-app.use("/orders", orderRoutes);
+app.use('/users', userRoutes);
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
 
-app.get("/", (req, res) => {
-	res.send("Cartify server is running...");
+app.get('/', (req, res) => {
+  res.send('Cartify server is running...');
 });
 
 // error handlers
